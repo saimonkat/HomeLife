5c128bbf7a1c290635f7edb0c22cf8a9fd457d43
Konstantin <kostyasupikos@gmail.com>
2020-05-03 00:45:16 +0300

    fixed index.html

--- server/index.js
+++ server/index.js
@@ -11,18 +11,6 @@ app.get('/', (req, response) => {
     response.sendFile(path.join(__dirname, '../app/index.html'));
 });
 
-app.get('/createEvent', (req, response) => {
-    response.end(`
-        <h1>About</h1>
-        <div>
-            <ul>
-                <li><a href="/">Home</a></li>
-                <li><a href="/about">About</a></li>
-            </ul>
-        </div>
-    `);
-});
-
 app.listen(PORT, () => {
     console.log('Server has been started...');
 })
