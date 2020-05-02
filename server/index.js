86f99a7ed897c8e767e7c522960c60b9a8844afd
Konstantin <kostyasupikos@gmail.com>
2020-05-03 00:37:39 +0300

    changed path

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const app = express();
 
 const PORT = process.env.PORT || 80;
 
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, '../app')));
 
 app.get('/', (req, response) => {
     response.sendFile(path.join(__dirname, '../app/index.html'));
