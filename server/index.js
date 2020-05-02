9c38596beab4335965f52a0a04e195e2f18f7ca6
Konstantin <kostyasupikos@gmail.com>
2020-05-03 01:57:05 +0300

    resolved conflicts

--- server/index.js
+++ server/index.js
@@ -8,19 +8,7 @@ const PORT = process.env.PORT || 80;
 app.use(express.static(path.join(__dirname, '../app')));
 
 app.get('/', (req, response) => {
-<<<<<<< HEAD
-    response.end(`
-    <h1>Home Hey Ho lets go</h1>
-    <div>
-        <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="/about">About</a></li>
-        </ul>
-    </div>
-    `);
-=======
     response.sendFile(path.join(__dirname, '../app/index.html'));
->>>>>>> b6f624a7b27367224adc9393df2b52c75a7ad7e8
 });
 
 app.listen(PORT, () => {
