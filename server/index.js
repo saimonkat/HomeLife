91a45a95dbaa31b35f01f6bb369d2f8b0bc05cc3
Konstantin <kostyasupikos@gmail.com>
2020-05-03 00:04:01 +0300

    added path to server

--- server/index.js
+++ server/index.js
@@ -1,20 +1,12 @@
 const express = require('express');
 
 const app = express();
+const path = require('path');
 
 const PORT = process.env.PORT || 80;
 
 app.get('/', (req, response) => {
-    response.se
-    response.end(`
-    <h1>Home Hey Ho lets go</h1>
-    <div>
-        <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="/about">About</a></li>
-        </ul>
-    </div>
-    `);
+    response.sendFile(path.join(__dirname, '../app/index.html'));
 });
 
 app.get('/createEvent', (req, response) => {
